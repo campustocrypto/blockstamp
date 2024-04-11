c966091e5e91f4d5c4061886b0ae6986e4296aa8
bharathkumaracharips <119281104+bharathkumaracharips@users.noreply.github.com>
2024-04-11 10:24:21 +0530

    Delete migrations directory

--- migrations/1_FacultyRegistry.js
+++
@@ -1,7 +0,0 @@
-// migrations/2_deploy_contracts.js
-
-const FacultyRegistry = artifacts.require("FacultyRegistry");
-
-module.exports = function(deployer) {
-  deployer.deploy(FacultyRegistry);
-};
